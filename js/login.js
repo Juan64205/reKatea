f0c19f9aec4cb396fe7b438572ce6006560a1819
FranklinZ12 <franklinq8@gmail.com>
2023-05-22 21:30:07 -0500

    Login con validacion terminado, no api

--- js/login.js
+++ js/login.js
@@ -1,11 +1,143 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+/*global $, document, window, setTimeout, navigator, console, location*/
+$(document).ready(function () {
 
-signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
-});
+    'use strict';
 
-signInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
+    var usernameError = true,
+        emailError = true,
+        passwordError = true,
+        passConfirm = true;
+
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+
+    // Detect browser for css purpose
+    if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+        $('.form form label').addClass('fontSwitch');
+    }
+
+    // Label effect
+    $('input').focus(function () {
+
+        $(this).siblings('label').addClass('active');
+    });
+
+    // validar formulario
+    $('input').blur(function () {
+
+        // Nombre
+        if ($(this).hasClass('name')) {
+            if ($(this).val().length === 0) {
+                $(this).siblings('span.error').text('Porfavor ingresar nombre completo').fadeIn().parent('.form-group').addClass('hasError');
+                usernameError = true;
+            } else if ($(this).val().length > 1 && $(this).val().length <= 6) {
+                $(this).siblings('span.error').text('Por favor escriba al menos 6 caracteres').fadeIn().parent('.form-group').addClass('hasError');
+                usernameError = true;
+            } else {
+                $(this).siblings('.error').text('').fadeOut().parent('.form-group').removeClass('hasError');
+                usernameError = false;
+            }
+        }
+        // Email
+        if ($(this).hasClass('email')) {
+            var email = $(this).val();
+
+            if ($(this).val().length == '') {
+                $(this).siblings('span.error').text('Por favor escriba su dirección de correo electrónico').fadeIn().parent('.form-group').addClass('hasError');
+                emailError = true;
+            } else if (!emailRegex.test(email)) {
+                $(this).siblings('span.error').text('Por favor, ingrese un correo electrónico válido').fadeIn().parent('.form-group').addClass('hasError');
+                emailError = true;
+            } else {
+                $(this).siblings('.error').text('').fadeOut().parent('.form-group').removeClass('hasError');
+                emailError = false;
+            }
+        }
+
+        // Contraseña
+        if ($(this).hasClass('pass')) {
+            var password = $(this).val();
+
+            if ($(this).val().length < 8) {
+                $(this).siblings('span.error').text('Por favor escriba al menos 8 caracteres').fadeIn().parent('.form-group').addClass('hasError');
+                passwordError = true;
+            } else if (!passwordRegex.test(password)) {
+                $(this).siblings('span.error').text('Incluya minúscula, una letra mayúscula y un dígito').fadeIn().parent('.form-group').addClass('hasError');
+                passwordError = true;
+            } else {
+                $(this).siblings('.error').text('').fadeOut().parent('.form-group').removeClass('hasError');
+                passwordError = false;
+            }
+        }
+
+        // Confirmar contraseña
+        if ($('.pass').val() !== $('.passConfirm').val()) {
+            $('.passConfirm').siblings('.error').text('Las contraseñas no coinciden').fadeIn().parent('.form-group').addClass('hasError');
+            passConfirm = false;
+        } else {
+            $('.passConfirm').siblings('.error').text('').fadeOut().parent('.form-group').removeClass('hasError');
+            passConfirm = false;
+        }
+
+        // label effect
+        if ($(this).val().length > 0) {
+            $(this).siblings('label').addClass('active');
+        } else {
+            $(this).siblings('label').removeClass('active');
+        }
+    });
+
+
+    // form switch
+    $('a.switch').click(function (e) {
+        $(this).toggleClass('active');
+        e.preventDefault();
+
+        if ($('a.switch').hasClass('active')) {
+            $(this).parents('.form-peice').addClass('switched').siblings('.form-peice').removeClass('switched');
+        } else {
+            $(this).parents('.form-peice').removeClass('switched').siblings('.form-peice').addClass('switched');
+        }
+    });
+
+
+    // Form submit
+    $('form.signup-form').submit(function (event) {
+        event.preventDefault();
+
+        if (usernameError == true || emailError == true || passwordError == true || passConfirm == true) {
+            $('.name, .email, .pass, .passConfirm').blur();
+        } else {
+            $('.signup, .login').addClass('switched');
+
+            setTimeout(function () { $('.signup, .login').hide(); }, 700);
+            setTimeout(function () { $('.brand').addClass('active'); }, 300);
+            setTimeout(function () { $('.heading').addClass('active'); }, 600);
+            setTimeout(function () { $('.success-msg p').addClass('active'); }, 900);
+            setTimeout(function () { $('.success-msg a').addClass('active'); }, 1050);
+            setTimeout(function () { $('.form').hide(); }, 700);
+        }
+    });
+
+    // Reload page
+    $('a.profile').on('click', function () {
+        location.reload(true);
+    });
+
+    // Form submit
+    $('form.login-form').submit(function (event) {
+        event.preventDefault();
+        if (emailError == true || passwordError == true) {
+            $('.email, .pass').blur();
+        } else {
+            // Resto del código anterior
+            console.log(SubmitEvent)
+            setTimeout(function () {
+                // Redirigir al usuario al enlace deseado
+                window.location.href = "/perfilVendedor.html";
+            }, 1500);
+        }
+    });
+
 });
